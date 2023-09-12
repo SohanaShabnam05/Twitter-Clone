fe24e1070e1353ad46509593b116b72eb9515b2a
sohana <sshabnamzsf@gmail.com>
2023-09-12 23:44:17 +0530

    interface changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import {BiHomeCircle, BiUser} from 'react-icons/bi'
-import {BsBell, BsBookmark, BsThreeDots, BsTwitter} from 'react-icons/bs'
+import {BsBell, BsBookmark, BsThreeDots, BsTwitter, BsDot} from 'react-icons/bs'
 import {HiOutlineHashtag} from 'react-icons/hi'
 import {BsEnvelope} from 'react-icons/bs'
 import Link from 'next/link'
@@ -33,6 +33,10 @@ const NAVIGATION_ITEMS = [
   {
     title: 'Profile',
     icon: BiUser
+  },
+  {
+    title: 'Dot',
+    icon: BsDot  
   }
 
 ]
@@ -45,8 +49,9 @@ export default function Home() {
 
     {/* Left side bar for navigation/header */}
     <LeftSidebar />
-    <main className="ml-[275px] py-6 mx-2 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600 px-6">
-    <h1 className="text-xl font-bold my-4 p-6">Home</h1>
+
+    <main className="ml-[275px] flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+    <h1 className="text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
     <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-strech py-4 space-x-2 border-gray-600 h-32 relative">
       <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
       <div className="flex flex-col w-full h-full">
@@ -63,6 +68,45 @@ export default function Home() {
         </div>
       </div>
       </div>
+    </div>
+    <div className="flex flex-col">
+{
+  Array.from({length:5}).map((_,i) => (
+    <div key={i} className="border-b-[0.5px] p-4 flex space-x-4">
+      <div>
+        <div className="w-10 h-10 bg-slate-200 rounded-full" />
+      </div>
+      <div className='flex flex-col space-y-4'> 
+          <div className="flex items-center space-x-1">
+            {/* 3 Divisons */}
+
+            <div className='font-bold'>Club Of Coders</div>
+            <div>@clubofcoderscom</div>
+            <div>
+              <BsDot />
+            </div>
+            <div>1 hour ago</div>
+
+          </div>
+          <div className='text-white text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa numquam quaerat architecto amet mollitia quo cumque odit possimus? Laboriosam possimus et ipsa, commodi pariatur consectetur quo vero ea adipisci unde exercitationem deserunt dolorem sequi. Maxime voluptas, voluptates, iure veritatis hic velit repudiandae vel iusto dignissimos sit necessitatibus. Alias, perspiciatis quo.
+          </div>
+          <div className='bg-slate-400 aspect-square w-full h-96 rounded-xl'>
+
+          </div>
+
+        {/* small last section */}
+        <div className='flex items-center space-x-2 w-full'>
+          <div>C</div>
+          <div>R</div>
+          <div>L</div>
+          <div>S</div>
+          <div>SH</div>
+        </div>
+
+        </div>
+         </div>
+  ))
+}
     </div>
     </main>
     {/* Render all things here */}
