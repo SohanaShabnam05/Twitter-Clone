6b35789e17c8f6dba3a8902575deda91796aff34
sohana <sshabnamzsf@gmail.com>
2023-10-08 20:19:04 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,9 +72,9 @@ export default function Home() {
     {/* Left side bar for navigation/header */}
     <LeftSidebar />
 
-    <main className="ml-[275px] flex w-full max-w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+    <main className="ml-[275px] flex w-full max-w-2xl h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
     <h1 className="text-xl font-bold p-6 backdrop-blur bg-black/10 sticky top-0">Home</h1>
-    <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-strech py-4 space-x-2 border-gray-600 h-32 relative">
+    <div className="border-t-[0.5px] px-4 border-b-[0.5px] flex items-strech py-6 space-x-2 border-gray-600 h-32 relative">
       <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
       <div className="flex flex-col w-full h-full">
 <input type="text" className="w-full h-full placeholder: text-xl placeholder:text-gray-500 bg-transparent border-b-[0.5px] outline-none border-none" placeholder="What's happening?"/>
@@ -84,7 +84,7 @@ export default function Home() {
 
         </div>
         <div className="w-full max-w-[100px]">
-              <button className="rounded-full m-4 bg-primary px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200">
+              <button className="rounded-full  bg-primary px-4 py-2 w-full text-lg text-center hover:bg-opacity-70 transition duration-200">
       Tweet
     </button>
         </div>
